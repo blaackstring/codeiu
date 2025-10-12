6351d7406aa7f2dae28d57b7faf42431211547e1
Arham Sheikh <arhamsheikh6851@gmail.com>
2025-10-13 01:39:07 +0530

    added layout

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,16 +1,10 @@
     'use client';
 
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { ChevronLeft } from 'lucide-react';
-import { cn } from '@/lib/utils';
-
 export default function MainLayout({ children }) {
-  const pathname = usePathname();
 
   return (
-    <AuthGuard>
+  
       <div className="min-h-screen bg-black text-white flex flex-col">
                
                 <div className="px-3 sm:px-4 md:px-6 py-6">
@@ -18,6 +12,6 @@ export default function MainLayout({ children }) {
                 </div>
               
       </div>
-    </AuthGuard>
+    
   );
 }
