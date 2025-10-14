f5314304324bc5d8cccc18a1fa4654965c94441e
wasif <syedwasifhussain123456@gmail.com>
2025-10-14 16:37:22 +0530

    updated authStore

--- src/app/store/useAuthStore.js
+++ src/app/store/useAuthStore.js
@@ -27,64 +27,7 @@ export const useAuthStore = create((set) => ({
       set({ isSigninUp: false });
     }
   },
-
-    signup:async (data) =>{
-        set({isSigninUp:true});
-        try {
-            const res = await axiosInstanceAuthService.post("/auth/register" , data);
-            
-             console.log(res);
-            
-         if(res.status===200)
-          {
-            set({authUser:res?.data?.data});
-           
-          toast.success(res?.data?.message || "signup successful")
-          return true
-          }
-
-        } catch (error) {
-            console.log("error signing up" , error);
-            toast.error("error signign up");
-        }
-
-        finally{
-            set({isSigninUp:false})
-        }
-    },
-
-    login: async (data) => {
-
-      console.log("data from login function ..............................");
-      console.log(data);
-      
-        set({isLoggingIn:true});
-        try {
-            const res = await axiosInstanceAuthService.post("/auth/login" , data);
-            console.log("responce from login function ..............................");
-            console.log(res.data);
-            
-            
-          if(res.success===true)
-          {
-              set({authUser:res?.data?.user});
-          
-              console.log(res);
-              toast.success(res?.data?.message || "login successful")
-              return true
-          }
-        
-        } catch (error) {
-            console.log("error loging In",error.message,error.response.data);
-            toast.error("error loging In")
-        }
-
-        finally{
-             set({isLoggingIn:false})
-        }
-
-    }, 
-
+ 
  
   logout: async (data) => {
     try {
@@ -124,15 +67,4 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  logout: async () => {
-    try {
-      await axiosInstanceAuthService.post("/auth/logout", null, { withCredentials: true });
-      set({ authUser: null, isAuthenticated: false });
-      toast.success("Logout successful");
-      return true;
-    } catch (e) {
-      toast.error(e?.response?.data?.message || "Error logging out");
-      return false;
-    }
-  },
 }));
